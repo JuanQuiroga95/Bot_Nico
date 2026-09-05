63a9e0fc30de08182737d94e5a76e6bb26d1932c
Juan Quiroga <juanpabloq_95@hotmail.com>
2026-09-05 10:55:04 -0300

    fix: resolver error de CommonJS export en ES modules para whatsapp-web.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { Client, LocalAuth } from 'whatsapp-web.js';
+import pkg from 'whatsapp-web.js';
+const { Client, LocalAuth } = pkg;
 import qrcode from 'qrcode-terminal';
 import axios from 'axios';
 import dotenv from 'dotenv';
