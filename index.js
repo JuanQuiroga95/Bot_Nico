f09d7b608cf19065f71b8601576be65351227e62
Juan Quiroga <juanpabloq_95@hotmail.com>
2026-09-06 17:20:35 -0300

    fix: set executablePath for puppeteer to work correctly on railway

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ const client = new Client({
     takeoverOnConflict: true,
     takeoverTimeoutMs: 0,
     puppeteer: {
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
         // Argumentos necesarios para que Puppeteer funcione en Railway sin interfaz grafica
         // y para que Chrome no gaste memoria en funciones que aqui no se usan.
         args: [
